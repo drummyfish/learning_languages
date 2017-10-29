14645071fe9eadbd3e10e43d91b4f1e01b816f72
Miloslav Číž <tastyfish@seznam.com>
2017-10-29 13:33:34 +0100

    ruby

--- ruby/showoff/main.rb
+++ ruby/showoff/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 $FACTORIAL_OF = 10
+$SORT_LENGTH = 10000
 
 class MyClass
   @@object_count = 0
@@ -12,7 +13,11 @@ class MyClass
     ObjectSpace.define_finalizer(self, method(:finalize))
   end
 
-  def get_name()
+  def print_info
+    #TODO
+  end
+
+  def get_name
     return @name
   end
 
@@ -39,11 +44,47 @@ def factorial_iterative(value)
   return result
 end
 
+def bubble_sort(data)
+  for i in (0...data.length)
+    for j in (0...data.length - i - 1)
+      if data[j] > data[j + 1]
+        data[j], data[j + 1] = data[j + 1], data[j]  # swap
+      end
+    end
+  end
+end
+
 #=========================== MAIN ==============================
 
-puts factorial_recursive($FACTORIAL_OF)
-puts factorial_iterative($FACTORIAL_OF)
+t1 = Time.now
+factorial_recursive($FACTORIAL_OF)
+t2 = Time.now
+puts "Factorial of #{$FACTORIAL_OF} recursively: #{t2 - t1} seconds."
+
+t1 = Time.now
+factorial_iterative($FACTORIAL_OF)
+t2 = Time.now
+puts "Factorial of #{$FACTORIAL_OF} iteratively: #{t2 - t1} seconds."
+
+begin
+  print "If I divide by zero... "
+  a = 10 / 0
+rescue Exception => e 
+  puts "there will be an exception: #{e.message}."
+end
 
 object1 = MyClass.new("object1")
+object1.print_info
 object2 = MyClass.new("object2")
 
+sort_data = Array.new
+
+#for i in (0...$SORT_LENGTH)
+#  sort_data.push(($SORT_LENGTH - i) % ($SORT_LENGTH / 4))
+#end
+
+#t1 = Time.now
+#bubble_sort(sort_data)
+#t2 = Time.now
+
+#puts "It took me #{t2 - t1} seconds to sort an array of length #{$SORT_LENGTH} with bubble sort."
