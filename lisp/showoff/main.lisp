b1b45d4e8a2e8c7a92014769b50336ec2b4562eb
Miloslav Číž <tastyfish@seznam.com>
2017-11-08 23:37:58 +0100

    lisp

--- lisp/showoff/main.lisp
+++ lisp/showoff/main.lisp
@@ -4,12 +4,22 @@
 ;;; WTFPL license
 
 (defconstant FACTORIAL_OF 10)
-(defconstant SORT_SIZE 10)
+(defconstant SORT_SIZE 100)
 
 (defun factorial (x)
   (if (< x 2)
     1
     (* x (factorial (- x 1)))
+  )
+)
+
+(defun bubble-sort (data)
+  (dotimes (i (array-dimension data 0))
+    (dotimes (j (array-dimension data 0))
+      (if (> (aref data j) (aref data i))
+        (rotatef (aref data j) (aref data i)) ; swap
+      )
+    )
   )
 )
 
@@ -22,8 +32,10 @@
 
 (setf sort-array (make-array SORT_SIZE))
 
-(dotimes (i (array-dimension sort-array 0))
-  (setf (aref sort-array i) i)
+(dotimes (i (array-dimension sort-array 0))   ; init the array
+  (setf (aref sort-array i) (mod (- SORT_SIZE i) (floor SORT_SIZE 4)))
 )
 
+(write-line (write-to-string sort-array))
+(bubble-sort sort-array)
 (write-line (write-to-string sort-array))
