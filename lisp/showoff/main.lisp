edb07b15d6432bd834c12276c02618bbcff78610
Miloslav Číž <tastyfish@seznam.com>
2017-11-19 00:22:21 +0100

    lisp

--- lisp/showoff/main.lisp
+++ lisp/showoff/main.lisp
@@ -6,6 +6,18 @@
 (defconstant FACTORIAL_OF 10)
 (defconstant SORT_SIZE 100)
 
+(defun print-header (title delim)
+  (write-line "")
+
+  (format t "~A~%" title)
+
+  (dotimes (i (length title))
+    (format t "~A" delim)
+  )
+
+  (write-line "")
+)
+
 (defun factorial-recursive (x)
   (if (< x 2)
     1
@@ -35,11 +47,21 @@
 
 ;--------------------------------------
 
+(print-header "Showing off the power of LISP!" #\~)
+(print-header "general" #\-)
+
 (write-line (write-to-string (factorial-recursive FACTORIAL_OF)))
 (write-line (write-to-string (factorial-iterative FACTORIAL_OF)))
 
 (write-line (write-to-string (get-decoded-time)))
 
+(format t "some random numbers from 1 to 10: ")
+
+(dotimes (i 10)
+  (format t "~D " (+ (random 10) 1))
+)
+
+(write-line (write-to-string (random 10)))
 
 (setf sort-array (make-array SORT_SIZE))
 
